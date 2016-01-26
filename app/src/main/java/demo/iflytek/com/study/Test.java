3b916bc73b639bbeb56b81cd67aaf2f741d66937
fangma <442333540@qq.com>
2016-01-26 18:11:51 +0800

    添加javaProject

--- app/src/main/java/demo/iflytek/com/study/Test.java
+++ app/src/main/java/demo/iflytek/com/study/Test.java
@@ -5,6 +5,6 @@ package demo.iflytek.com.study;
  */
 public class Test {
     public static void main(String args[]){
-
+        System.out.println("我的lib");
     }
 }
--- myprojectlib/src/main/java/com/example/MyClass.java
+++ myprojectlib/src/main/java/com/example/MyClass.java
@@ -1,4 +1,7 @@
 package com.example;
 
 public class MyClass {
+    public static void main(String args[]){
+        System.out.println("我的lib");
+    }
 }
